96261a29a07d6a8ffd96ed56eca4884799212b68
Vincent Lewi <vincentlewii@gmail.com>
2022-09-27 10:25:12 +0800

    new 3d houses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { findByPlaceholderText } from '@testing-library/react';
 import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // Change with time
   // const [count, setCount] = useState(1)
 
   // useEffect(() => {
@@ -20,38 +22,39 @@ function App() {
     return images;
   }
   
-  const images = importAll(require.context('./house_compressed', false, /\.(png|jpe?g|svg)$/));
+  const images = importAll(require.context('./house', false, /\.(png|jpe?g|svg)$/));
 
   const [scrolled, setScrolled] = useState(1)
 
   useEffect(() => {
     window.addEventListener('scroll', scrollProgress)
-
     return () => window.removeEventListener('scroll', scrollProgress)
   })
 
   const scrollProgress = () => {
     const scrollPx = document.documentElement.scrollTop
-    const winHeightPx = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    const winHeightPx = document.getElementsByClassName('App-header')[0].scrollHeight - document.documentElement.clientHeight
 
-    const scrollLen = Math.ceil(scrollPx / winHeightPx * 100) + 1
-
+    const scrollLen = Math.min(120, Math.ceil(scrollPx / winHeightPx * 100) + 1)
     setScrolled(scrollLen)
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <div class='main'>
-          <div class='login'>
+        <div className='main'>
+          {/* <div class='login'>
             <h1>myPSR</h1>
             <h3>home away from home</h3>
-          </div>
-          <img src={images[`${scrolled.toString().padStart(4, 0)}.jpg`]} />
+          </div> */}
+          <img src={images[`${scrolled.toString().padStart(4, 0)}.jpg`]} /> 
         </div>
       </header>
-      <div class='content'>
-        test
+      <div className='testdiv'>
+        <img src={require("./house_transparent/0001.png")} className='testimg'/>
+      </div>
+      <div className='content'>
+        <h1>test</h1>
       </div>
     </div>
   );
