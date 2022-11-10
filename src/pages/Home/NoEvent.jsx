f1c15dcb3db43a7c549bea34d29b6609b1a8857a
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2022-11-10 16:54:17 +0800

    Fixed events home page
    Added lottifiles and correctly displays the events

--- src/pages/Home/NoEvent.jsx
+++ src/pages/Home/NoEvent.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import Lottie from "react-lottie";
+import animationData from "./104954-calendar.json"
 
 
 
@@ -6,9 +8,19 @@ import { useNavigate } from "react-router-dom";
 export default function NoEvent(){
     console.log("==RENDER in NoEvent.jsx==")
     const navigate = useNavigate()
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+
     return(
         <div className="noevent">
-            <img src= {require('./schedule.png')} alt="No schedule" style = {{width: 200}}/>
+            <Lottie options={defaultOptions} height={400} width={400}/>
             <h2>You have not joined any events</h2>
             <button className="createbtn" onClick={() => {
                 navigate("/mypsr/events")
