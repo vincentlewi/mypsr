dc06942d56e577d21a5e0b5ff1d479774bef6e80
Vincent Lewi <vincentlewii@gmail.com>
2022-11-13 13:14:28 +0800

    standardized 24h format, fixed lastMessage bug

--- src/pages/Chats/components/Input.jsx
+++ src/pages/Chats/components/Input.jsx
@@ -143,7 +143,6 @@ const Input = () => {
 
   const handleKey = (e) => {
     e.code === "Enter" && handleSend();
-    console.log("WORKING")
   };
 
   useEffect(() =>  {
@@ -158,7 +157,7 @@ const Input = () => {
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            console.log(downloadURL)
+            
             await updateDoc(doc(db, "chats", data.chatId), {
               messages: arrayUnion({
                 id: uuid(),
@@ -178,20 +177,17 @@ const Input = () => {
         
             await updateDoc(doc(db, "userChats", data.user.uid), {
               [data.chatId + ".lastMessage"]: {
-                text: 'sent you a picture',
-                img:null
+                text: 'sent you a picture':null
               },
               [data.chatId + ".date"]: serverTimestamp(),
             });
-            console.log('pic snet')
           });
         }
       );
     }
-    // console.log(img)
-      setText("");
-      setImg(null);
-    }, [img])
+    setText("");
+    setImg(null);
+  }, [img])
   // console.log(data.user.uid)
   // updateDoc(doc(db, "userChats", user.uid), {
   //       [data.chatId + ".lastMessage"]: {
--- src/pages/Chats/components/Message.jsx
+++ src/pages/Chats/components/Message.jsx
@@ -35,8 +35,8 @@ const Message = ({ message }) => {
           {message.text && message.text}
           {message.img && <img src={message.img} alt="" />}
           <div className="timestamp">{today.toDateString() === tanggal.toDateString()
-            ? format(tanggal, 'HH:mm aa')
-            : format(tanggal, 'MMM dd, HH:mm aa')
+            ? format(tanggal, 'HH:mm')
+            : format(tanggal, 'MMM dd, HH:mm')
           }</div>
         </div>
       </div>
